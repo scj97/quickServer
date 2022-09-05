321b4ae79533e49582e76fa227370952c4b959eb
Sven Jaspers <47367774+scj97@users.noreply.github.com>
2022-09-05 14:05:29 +0200

    Update QuickStart.sh

--- QuickStart.sh
+++ QuickStart.sh
@@ -3,20 +3,25 @@
 sudo apt update 
 sudo apt upgrade -y
 
+#.bashrc
 rm ~/.bashrc
 mv .bashrc ~/.bashrc
 source ~/.bashrc
 
-sudo apt install git neovim -y
+sudo apt install git neovim docker.io docker-compose -y
 
+# Vim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-       
+
+# init.vim
 mkdir ~/.config
 mkdir ~/.config/nvim
 mv init.vim ~/.config/nvim/init.vim
 
-sudo apt install docker.io docker-compose -y
+# docker-compose file
+mv docker-compose.yaml ~/docker-compose.yaml
 
+# clean up
 cd ~
 rm -rf quickServer
